0ec7a2632243720f9a10f5ef665a2333f56d20d4
Potato Wu <z.wu233@gmail.com>
2020-04-27 23:08:22 +0800

    Added basic animal animations and mouse hover trigger

---
+++ Assets/Scripts/Animal_MouseBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animal_MouseBehaviour : MonoBehaviour
+{
+    public Animator animator;
+
+
+    void OnMouseEnter()
+    {
+        Debug.Log("Mouse entered!");
+        animator.SetBool("IsHover", true);
+    }
+
+    void OnMouseExit ()
+    {
+        Debug.Log("Mouse exit!");
+        animator.SetBool("IsHover", false);
+    }
+}
