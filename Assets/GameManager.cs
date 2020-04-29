2aa89bb80964f9871808927e99f8be4079279d02
Potato Wu <z.wu233@gmail.com>
2020-04-29 14:44:12 +0800

    Level 1 Art & basic game manager

---
+++ Assets/GameManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject[] level;
+    public GameObject[] levelUI;
+
+    public GameObject mainMenu;
+    public GameObject mainHUD;
+    public GameObject nextLevelUI;
+    public GameObject levelBeginUI;
+    public GameObject gameOverUI;
+
+    public int currentLevel;
+    public bool isGameOver;
+
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CatFound()
+    {
+        
+    }
+
+    public void GameWin()
+    {
+
+    }
+
+    public void GameOver()
+    {
+
+    }
+
+    public void ClearGameData()
+    {
+
+    }
+}
