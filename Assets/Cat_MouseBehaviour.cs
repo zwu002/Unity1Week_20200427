92a1e258a8eaafc262f6d600016b8233f2806892
Potato Wu <z.wu233@gmail.com>
2020-04-29 16:47:50 +0800

    Game Win UI

---
+++ Assets/Cat_MouseBehaviour.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cat_MouseBehaviour : MonoBehaviour
+{
+
+    void OnMouseDown()
+    {
+        Debug.Log("Cat clicked!");
+
+        GameManager.GetInstance().CatFound();
+    }
+}
--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -10,38 +10,80 @@ public class GameManager : MonoBehaviour
     public GameObject mainMenu;
     public GameObject mainHUD;
     public GameObject nextLevelUI;
-    public GameObject levelBeginUI;
     public GameObject gameOverUI;
+    public GameObject gameWinUI;
 
     public int currentLevel;
+    public int totalLevel;
+    public bool isCatFound;
     public bool isGameOver;
 
+    static GameManager instance = null;
 
-
-    void Start()
+    void Awake()
     {
-        
+        Debug.Log("Game Manager Awake");
+        if (null == instance)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Debug.LogWarning("Destroy duplicate gm");
+            Destroy(gameObject);
+        }
+
+        currentLevel = 0;
+
+        level[currentLevel].SetActive(true);
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public static GameManager GetInstance()
     {
-        
+        return instance;
     }
 
     public void CatFound()
     {
-        
+        Debug.Log("Cat Found!");
+        isCatFound = true;
+
+        currentLevel++;
+        if (currentLevel < totalLevel)
+        {
+            Debug.Log("Next level Begin!");
+            nextLevelUI.SetActive(true);
+        }
+        else
+        {
+            GameWin();
+        }
+
     }
 
     public void GameWin()
     {
-
+        Debug.Log("You Won!");
+        gameWinUI.SetActive(true);
     }
 
     public void GameOver()
     {
+        Debug.Log("Game Over!");
+        gameOverUI.SetActive(true);
+    }
 
+    public void RestartGame()
+    {
+        ClearGameData();
+        level[currentLevel].SetActive(true);
+    }
+
+    public void NextLevelBegin()
+    {
+        nextLevelUI.SetActive(false);
+        levelUI[currentLevel].SetActive(true);
     }
 
     public void ClearGameData()
